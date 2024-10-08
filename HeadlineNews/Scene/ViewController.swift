19b82584694688f7f7840e7b6cbbc9bf386e62db
dev-hhn0212 <hhn0212@sojung.design>
2024-10-08 10:53:44 +0900

    프로젝트 초기 세팅

---
+++ HeadlineNews/Scene/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeadlineNews
+//
+//  Created by 김지훈 on 10/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
