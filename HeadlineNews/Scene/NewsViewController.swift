692170db97215811eb9a5e0ea9dd01251906c2ee
dev-hhn0212 <hhn0212@sojung.design>
2024-10-09 17:59:28 +0900

    [FEAT] newsCollectionView 세로, 가로 모드일 때 변경되는 UI 적용

--- HeadlineNews/Scene/NewsViewController.swift
+++ HeadlineNews/Scene/NewsViewController.swift
@@ -15,12 +15,16 @@ class NewsViewController: UIViewController {
     
     // MARK: - Components
     var newsItems = Array(1...37).map { "\($0)" }
-
+    
     let newsCollectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex, environment) -> NSCollectionLayoutSection? in
+            return NewsViewController.createSectionLayout(for: environment)
+        }
         let newsCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         newsCollectionView.register(NewsCell.self, forCellWithReuseIdentifier: NewsCell.identifier)
         newsCollectionView.backgroundColor = .yellow
+        newsCollectionView.showsVerticalScrollIndicator = false
+        newsCollectionView.showsHorizontalScrollIndicator = false
         return newsCollectionView
     }()
 
@@ -44,6 +48,11 @@ extension NewsViewController {
         navigationUI()
         setUp()
     }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        newsCollectionView.collectionViewLayout.invalidateLayout()
+    }
 }
 
 // MARK: - Navigation
@@ -51,7 +60,7 @@ extension NewsViewController {
     func navigationUI() {
         navigationController?.navigationBar.barTintColor = .background.white
         
-        let viewTitle = CustomLabel(title: "News", size: Constants.size.size24, weight: .SemiBold, color: .text.black)
+        let viewTitle = CustomLabel(title: "News", size: Constants.size.size28, weight: .SemiBold, color: .text.black)
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: viewTitle)
     }
 }
@@ -71,23 +80,57 @@ private extension NewsViewController {
 
 // MARK: - Method
 private extension NewsViewController {
-    
+    static func createSectionLayout(for environment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection {
+        let isLandscape = environment.container.effectiveContentSize.width > environment.container.effectiveContentSize.height
+        
+        if isLandscape {
+            // 가로 모드
+            let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(Constants.size.size300), heightDimension: .absolute(Constants.size.size120))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(Constants.size.size300 * 5 + 20), heightDimension: .absolute(Constants.size.size120))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+            group.interItemSpacing = .fixed(5)
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 5, trailing: 0)
+            section.orthogonalScrollingBehavior = .continuous
+            
+            return section
+        } else {
+            // 세로 모드
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(Constants.size.size120))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 5, bottom: 5, trailing: 5)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(Constants.size.size120))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+            
+            let section = NSCollectionLayoutSection(group: group)
+            return section
+        }
+    }
 }
 
 // MARK: - Delegate
 extension NewsViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return newsItems.count
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return Int(ceil(Double(newsItems.count) / 5.0))
     }
     
-    // 셀 설정
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let startIndex = section * 5
+        let endIndex = min(startIndex + 5, newsItems.count)
+        return endIndex - startIndex
+    }
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NewsCell.identifier, for: indexPath) as? NewsCell else { return UICollectionViewCell() }
         
+        let itemIndex = indexPath.section * 5 + indexPath.row
         cell.backgroundColor = .lightGray
-        
-        let numberText = "\(indexPath.row + 1)"
-        cell.label.text = numberText
+        cell.label.text = newsItems[itemIndex]
         
         return cell
     }
